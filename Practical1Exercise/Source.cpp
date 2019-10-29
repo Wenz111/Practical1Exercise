ab9eb23c923641759475f87d4c62f586f82206cf
Wenz11 <angzw-wm17@student.tarc.edu.my>
2019-10-29 15:55:32 +0800

    Added Functions

--- Practical1Exercise/Source.cpp
+++ Practical1Exercise/Source.cpp
@@ -1,86 +1,12 @@
 #include <Windows.h>
 #include <gl/GL.h>
+#include <math.h>
 
 #pragma comment (lib, "OpenGL32.lib")
 
-#define WINDOW_TITLE "Demo"
+#define WINDOW_TITLE "Practical 1 Exercise"
 
-
-LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-
-	case WM_KEYDOWN:
-		if (wParam == VK_ESCAPE) PostQuitMessage(0);
-		break;
-
-	default:
-		break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
-//--------------------------------------------------------------------
-
-bool initPixelFormat(HDC hdc)
-{
-	PIXELFORMATDESCRIPTOR pfd;
-	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
-
-	pfd.cAlphaBits = 8;
-	pfd.cColorBits = 32;
-	pfd.cDepthBits = 24;
-	pfd.cStencilBits = 0;
-
-	pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
-
-	pfd.iLayerType = PFD_MAIN_PLANE;
-	pfd.iPixelType = PFD_TYPE_RGBA;
-	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
-	pfd.nVersion = 1;
-
-	// choose pixel format returns the number most similar pixel format available
-	int n = ChoosePixelFormat(hdc, &pfd);
-
-	// set pixel format returns whether it sucessfully set the pixel format
-	if (SetPixelFormat(hdc, n, &pfd))
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-//--------------------------------------------------------------------
-
-void display()
-{
-	//--------------------------------
-	//	OpenGL drawing
-	//--------------------------------
-
-	glClearColor(1.0f, 228.0/255.0f, 204.0/255.0f, 0.9f);
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glBegin(GL_QUADS);
-	// Grey Box
-	glColor3f(79.0 / 255.0f, 79.0 / 255.0f, 79.0 / 255.0f);
-	glVertex2f(-0.8f, -0.8f);
-	glVertex2f(0.8f, -0.8f);
-	glVertex2f(0.8f, 0.8f);
-	glVertex2f(-0.8f, 0.8f);
-	glEnd();
-
-	//--------------------------------
-	//	End of OpenGL drawing
-	//--------------------------------
-}
-//--------------------------------------------------------------------
+int question;
 
 void drawPahangFlag() {
 	glClearColor(1.0f, 228.0 / 255.0f, 204.0 / 255.0f, 0.9f);
@@ -102,7 +28,7 @@ void drawPahangFlag() {
 	glVertex2f(0.5f, 0.1f);
 	glVertex2f(-0.5f, 0.1f);
 	glEnd();
-	}
+}
 
 void drawNegeriSembilangFlag() {
 	glClearColor(1.0f, 228.0 / 255.0f, 204.0 / 255.0f, 0.9f);
@@ -243,6 +169,174 @@ void drawScotlandFlag() {
 	glVertex2f(0.8f, 0.4f);
 	glEnd();
 }
+
+void drawJapanFlag() {
+	glClearColor(1.0f, 228.0 / 255.0f, 204.0 / 255.0f, 0.9f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_QUADS);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.5f, 0.3f);
+	glVertex2f(-0.5f, 0.3f);
+	glVertex2f(-0.5f, -0.3f);
+	glVertex2f(0.5f, -0.3f);
+	glEnd();
+
+	float x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0;
+	float angle = 0.0;
+	float radius = 0.2;
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0.0f, 0.0f);
+
+	for (angle = 0; angle <= 360; angle += 0.2) {
+		x2 = x1 + cos(angle) * radius;
+		y2 = y1 + sin(angle) * radius;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+}
+
+LRESULT WINAPI WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE) {
+			PostQuitMessage(0);
+			break;
+		}
+		if (wParam == 0x31) {
+			question = 1;
+			break;
+		}
+		if (wParam == 0x32) {
+			question = 2;
+			break;
+		}
+		if (wParam == 0x33) {
+			question = 3;
+			break;
+		}
+		if (wParam == 0x34) {
+			question = 4;
+			break;
+		}
+		if (wParam == 0x35) {
+			question = 5;
+			break;
+		}
+		if (wParam == 0x36) {
+			question = 6;
+			break;
+		}
+
+	default:
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+//--------------------------------------------------------------------
+
+bool initPixelFormat(HDC hdc)
+{
+	PIXELFORMATDESCRIPTOR pfd;
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.cAlphaBits = 8;
+	pfd.cColorBits = 32;
+	pfd.cDepthBits = 24;
+	pfd.cStencilBits = 0;
+
+	pfd.dwFlags = PFD_DOUBLEBUFFER | PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW;
+
+	pfd.iLayerType = PFD_MAIN_PLANE;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+
+	// choose pixel format returns the number most similar pixel format available
+	int n = ChoosePixelFormat(hdc, &pfd);
+
+	// set pixel format returns whether it sucessfully set the pixel format
+	if (SetPixelFormat(hdc, n, &pfd))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+//--------------------------------------------------------------------
+
+void display()
+{
+	//--------------------------------
+	//	OpenGL drawing
+	//--------------------------------
+
+	//switch (question)
+	//{
+	//case 1:
+	//	drawPahangFlag();
+	//	break;
+	//case 2:
+	//	drawNegeriSembilangFlag();
+	//	break;
+	//case 3:
+	//	drawEnglandFlag();
+	//	break;
+	//case 4:
+	//	drawScotlandFlag();
+	//	break;
+	//case 5:
+	//	drawJapanFlag();
+	//	break;
+	//case 6:
+	//	break;
+	//default:
+	//	break;
+	//}
+
+	glClearColor(1.0f, 228.0/255.0f, 204.0/255.0f, 0.9f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_QUADS);
+	// Grey Box
+	glColor3f(79.0 / 255.0f, 79.0 / 255.0f, 79.0 / 255.0f);
+	glVertex2f(-0.8f, -0.8f);
+	glVertex2f(0.8f, -0.8f);
+	glVertex2f(0.8f, 0.8f);
+	glVertex2f(-0.8f, 0.8f);
+	glEnd();
+
+	float x1 = 0.0, y1 = 0.0, x2 = 0.0, y2 = 0.0;
+	float angle = 0.0;
+	float radius = 0.2;
+
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0.0f, 0.0f);
+
+	for (angle = 0; angle <= 360; angle += 0.2) {
+		x2 = x1 + cos(angle) * radius;
+		y2 = y1 + sin(angle) * radius;
+		glVertex2f(x2, y2);
+	}
+	glEnd();
+
+	//--------------------------------
+	//	End of OpenGL drawing
+	//--------------------------------
+}
+//--------------------------------------------------------------------
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow)
 {
